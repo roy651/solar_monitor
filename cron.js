c62d241ddb742df481fe352eb26f867f5bbb1adc
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2023-03-19 16:56:43 +0200

    add cron+fix

--- cron.js
+++ cron.js
@@ -3,6 +3,7 @@ const cron = require("node-cron");
 
 // every day at 10AM
 // cron.schedule(`0 10 * * */1`, async () => {
+
 // every minute
 cron.schedule(`* * * * */1`, async () => {
   console.log(`running daily...`);
