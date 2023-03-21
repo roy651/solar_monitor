4cbac81eee70a7162caf6bbba06d4d07b95f36db
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2023-03-21 17:43:13 +0200

    gmt+2

--- cron.js
+++ cron.js
@@ -4,8 +4,8 @@ const cron = require("node-cron");
 // At every minute
 // cron.schedule(`* * * * *`, async () => {
 
-// At minute 0 past every 2nd hour from 10 through 16.
-cron.schedule(`0 10-16/2 * * *`, async () => {
+// At minute 0 past every 2nd hour from 10 through 16 (GMT+2).
+cron.schedule(`0 8-14/2 * * *`, async () => {
   console.log(`running daily...`);
   app.runDaily();
   console.log(`daily ended.`);
